2c1d6690a89c50b40e10bf6811dd39f97af41990
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2025-01-18 23:33:26 +0100

    full dataset

--- src/symmetricflow/main.py
+++ src/symmetricflow/main.py
@@ -5,7 +5,7 @@ from models.SymmFM import SymmFM
 if __name__ == '__main__':
     args = parse_args_SymmetricFlowMatching()
     image_shape, channels, dataloader = celeb_hq_masked_dataloader(1, 0, 'train', args.size)
-    _, _, dataloader_val = celeb_hq_masked_dataloader(16, 0, 'val', args.size)
+    _, _, dataloader_val = celeb_hq_masked_dataloader(16, 0, 'validation', args.size)
 
     model = SymmFM(args, image_shape, channels)
     model.train_model(dataloader, dataloader_val)
