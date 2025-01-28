de57d9e0fb9548b6ea9215eecdba4f21e54f5b5a
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2025-01-28 21:56:20 +0100

    cityscapes

--- src/symmetricflow/main.py
+++ src/symmetricflow/main.py
@@ -1,12 +1,16 @@
-from data.Dataloaders import celeb_hq_masked_dataloader
+from data.Dataloaders import celeb_hq_masked_dataloader, cityscapes_dataloader
 from utils.util import parse_args_SymmetricFlowMatching
 from models.SymmFM import SymmFM
 import torch
 
 if __name__ == '__main__':
     args = parse_args_SymmetricFlowMatching()
-    image_shape, channels, dataloader = celeb_hq_masked_dataloader(args.batch_size, args.num_workers, 'train', args.size)
-    _, _, dataloader_val = celeb_hq_masked_dataloader(16, args.num_workers, 'validation', args.size)
+    if args.dataset == 'celeba':
+        image_shape, channels, dataloader = celeb_hq_masked_dataloader(args.batch_size, args.num_workers, 'train', args.size)
+        _, _, dataloader_val = celeb_hq_masked_dataloader(16, args.num_workers, 'validation', args.size)
+    else:
+        image_shape, channels, dataloader = cityscapes_dataloader(args.batch_size, args.num_workers, 'train'. args.size)
+        _, _, dataloader_val = cityscapes_dataloader(16, args.num_workers, 'validation', args.size)
 
     model = SymmFM(args, image_shape, channels)
     model.load_checkpoint(args.checkpoint)
