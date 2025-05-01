f896ab15c711e8c77036063a3125f59c1d511d87
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2025-05-01 19:04:31 +0200

    vae finetune with predictions

--- src/symmetricflow/models/SymmFM.py
+++ src/symmetricflow/models/SymmFM.py
@@ -1230,7 +1230,7 @@ class SymmFM(nn.Module):
         :param dataloader: data loader
         '''
         self.model.eval()
-        #self.vae.decoder.load_state_dict(torch.load(os.path.join(models_dir, 'vae_decoder_step_4400_coco_384_mse.pt'), weights_only=False))
+        self.vae.decoder.load_state_dict(torch.load(os.path.join(models_dir, 'vae_decoder_step_5000_coco_384_mse.pt'), weights_only=False))
         self.vae.eval()
         gt = []
         pred = []
