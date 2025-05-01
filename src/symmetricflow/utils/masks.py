41c6cf637d43a9433fe76695987880bae355fab7
caetas <f.t.de.espirito.santo.e.caetano@tue.nl>
2025-05-01 17:18:14 +0200

    vae finetune with predictions

--- src/symmetricflow/utils/masks.py
+++ src/symmetricflow/utils/masks.py
@@ -45,6 +45,7 @@ def mask_to_class(masks, dataset):
 
     elif dataset == 'coco':
         color_list = build_palette(6, 50)  # Assuming build_palette returns a list of RGB colors
+        print(len(color_list))
         color_list = color_list[:171] + color_list[-1:]  # Trim specific indices
         color_list = torch.tensor(color_list, dtype=torch.float32, device=masks.device)  # Convert to tenso
     
